dd3dcb6135ed5f2e74f9404d161996d29c9f08a4
Scott Bouchard <sb@mbp.local>
2020-11-14 17:21:47 -0500

    added facebook

--- run-cron.sh
+++ run-cron.sh
@@ -12,6 +12,7 @@ echo "BASH_ENV=${ENV_VARS_FILE}" >> ${CRON_FILE}
 echo "* * * * * echo 'Cron is alive!' > /proc/1/fd/1 2>/proc/1/fd/2" >> ${CRON_FILE}
 echo "0 * * * * cd /usr/src/app && /usr/local/bin/scrapy crawl pinkbike > /proc/1/fd/1 2>/proc/1/fd/2" >> ${CRON_FILE}
 echo "0 * * * * cd /usr/src/app && /usr/local/bin/scrapy crawl craigslist > /proc/1/fd/1 2>/proc/1/fd/2" >> ${CRON_FILE}
+echo "0 * * * * cd /usr/src/app && /usr/local/bin/scrapy crawl facebook > /proc/1/fd/1 2>/proc/1/fd/2" >> ${CRON_FILE}
 echo "#empty line" >> ${CRON_FILE}
 
 #installing cron schedule
